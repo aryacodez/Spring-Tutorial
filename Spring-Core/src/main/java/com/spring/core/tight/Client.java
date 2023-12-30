ce357f96fa084a195aa54e28b9a72781201c602e
aryacodez <arya.08.as44@gmail.com>
2023-12-30 18:45:03 +0530

    Spring Core

--- Spring-Core/src/main/java/com/spring/core/tight/Client.java
+++ Spring-Core/src/main/java/com/spring/core/tight/Client.java
@@ -1,10 +1,22 @@
 package com.spring.core.tight;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
     public static void main(String[] args){
         Vehicle vehicle = new Bike();
 
         Traveler traveler = new Traveler(vehicle);
         traveler.start();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(config.class);
+
+        // Fetch Spring bean (object) from Spring IOC container
+        Car car = applicationContext.getBean(Car.class);
+        car.move();
+
+        Bike bike = applicationContext.getBean(Bike.class);
+        bike.move();
     }
 }
